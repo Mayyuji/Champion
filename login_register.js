9fb0b9185c9ddb115403d8910b02615826a74e5d
Mayyuji <2455626085@qq.com>
2020-10-09 20:53:56 +0800

    day10/9bulid

---
+++ login_register.js
@@ -0,0 +1,76 @@
+define(['jquery', "jquery-cookie"], function ($) {
+
+
+    function register() {
+
+        $("#register").click(function () {
+            $.ajax({
+                type: "post",
+                url: "./php/register.php",
+                data: {
+                    username: $("#username").val(),
+                    password: $("#password").val(),
+                    rpassword: $("#rpassword").val(),
+                },
+                success: function (result) {
+                    if(JSON.parse(result).code){
+                        var str=`<span>X</span>${JSON.parse(result).msg}&nbsp;;&nbsp;&nbsp;
+                  返回码:${JSON.parse(result).code}
+                   `
+                   $(".alert-danger").html(str).css({'display':'block'});
+                   }else{
+                       $(window).attr('location','login.html');
+                   }
+                },
+                error: function (e) {
+                    console.log(e);
+                }
+
+            })
+        })
+    }
+
+    function Login() {
+        $("#login").click(function () {
+            $.ajax({
+                type: "post",
+                url: "./php/login.php",
+                data: {
+                    username: $("#username").val(),
+                    password: $("#password").val()
+                },
+                success: function (result) {
+                    if(JSON.parse(result).code){
+                         var str=`<span>X</span>${JSON.parse(result).msg}&nbsp;;&nbsp;&nbsp;
+                   返回码:${JSON.parse(result).code}
+                    `
+                    $(".alert-danger").html(str).css({'display':'block'});
+                    }else{
+                        $(window).attr('location','index.html');
+                    }
+                   
+                },
+                error: function (e) {
+                    console.log(e);
+                }
+
+            })
+        })
+    }
+    function clearInput(){
+        var dis=$(".alert-danger").css('display')
+         if(dis='block'){
+            $("input").focus(function(){
+                $(".alert-danger").css({
+                    'display':'none'
+                });
+                $("input").val('');
+            })
+         }
+    }
+    return {
+        register: register,
+        Login: Login,
+        clearInput:clearInput,
+    }
+});
